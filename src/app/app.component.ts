76a7aa8265ef1f4d84d7069c66ec554b05ddf853
JonathanRios18 <sikra182@gmail.com>
2025-03-17 16:44:07 -0600

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component'; // Asegúrate de la ruta correcta
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [RouterModule, NavbarComponent], // Agregar RouterModule y NavbarComponent
+  imports: [RouterModule], // Agregar RouterModule y NavbarComponent
 })
 export class AppComponent {}
 
