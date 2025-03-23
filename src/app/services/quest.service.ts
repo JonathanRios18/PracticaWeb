85795449eeb18f2c2b9a4061328e7c71f5c2c9ec
JonathanRios18 <sikra182@gmail.com>
2025-03-23 05:10:01 -0600

    Quests

--- src/app/services/quest.service.ts
+++ src/app/services/quest.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Quest {
+  id: number;
+  title: string;
+  description: string;
+  dificulty_level: string;
+  character: string;
+  enemy: string;
+  nation: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class QuestService {
+  private apiUrl = 'http://192.168.100.194:8000/api/quests';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getQuests(): Observable<Quest[]> {
+    return this.http.get<Quest[]>(this.apiUrl);
+  }
+
+  getQuest(id: number): Observable<Quest> {
+    return this.http.get<Quest>(`${this.apiUrl}/${id}`);
+  }
 }
