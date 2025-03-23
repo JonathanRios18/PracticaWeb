c04023aaefe93ec5d3efda1fa448303f43123f49
JonathanRios18 <sikra182@gmail.com>
2025-03-23 00:31:08 -0600

    Generates

---
+++ src/app/auth/enemies/enemies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-enemies',
+  imports: [],
+  templateUrl: './enemies.component.html',
+  styleUrl: './enemies.component.css'
+})
+export class EnemiesComponent {
+
+}
---
+++ src/app/auth/nations/nations.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nations',
+  imports: [],
+  templateUrl: './nations.component.html',
+  styleUrl: './nations.component.css'
+})
+export class NationsComponent {
+
+}
