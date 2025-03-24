2c916eedeb2f387b40fa2ead0b2fb76cea80127e
JonathanRios18 <sikra182@gmail.com>
2025-03-23 23:05:38 -0600

    Falto el quest

--- src/app/services/quest.service.ts
+++ src/app/services/quest.service.ts
@@ -2,14 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-export interface Quest {
+export interface QuestDisplay {
   id: number;
   title: string;
   description: string;
   dificulty_level: string;
-  character: string;
-  enemy: string;
-  nation: string;
+  character: string; // Nombre del personaje
+  enemy: string; // Nombre del enemigo
+  nation: string; // Nombre de la nación
+}
+
+export interface QuestForm {
+  id: number;
+  title: string;
+  description: string;
+  dificulty_level: string;
+  character_id: number; // ID del personaje
+  enemy_id: number; // ID del enemigo
+  nation_id: number; // ID de la nación
 }
 
 @Injectable({
@@ -20,11 +30,15 @@ export class QuestService {
 
   constructor(private http: HttpClient) {}
 
-  getQuests(): Observable<Quest[]> {
-    return this.http.get<Quest[]>(this.apiUrl);
+  getQuests(): Observable<QuestDisplay[]> {
+    return this.http.get<QuestDisplay[]>(this.apiUrl);
   }
 
-  getQuest(id: number): Observable<Quest> {
-    return this.http.get<Quest>(`${this.apiUrl}/${id}`);
+  getQuest(id: number): Observable<QuestDisplay> {
+    return this.http.get<QuestDisplay>(`${this.apiUrl}/${id}`);
+  }
+
+  addQuest(quest: QuestForm): Observable<QuestForm> {
+    return this.http.post<QuestForm>(this.apiUrl, quest);
   }
 }
